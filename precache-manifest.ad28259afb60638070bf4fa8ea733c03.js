bf42f5427a8df4fc3420b0ae5adbfede35a028fe
Newgin-sam <newginsam@gmail.com>
2020-05-13 11:46:58 +0530

    Updates

---
+++ precache-manifest.ad28259afb60638070bf4fa8ea733c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8efffe60e96d7b043f5a4258cd8b9b",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "789c6506f3b7357dc4fc",
+    "url": "/monsters-roledex/static/css/main.3636a095.chunk.css"
+  },
+  {
+    "revision": "acb4f4ef9cd8d446d668",
+    "url": "/monsters-roledex/static/js/2.fff03364.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.fff03364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789c6506f3b7357dc4fc",
+    "url": "/monsters-roledex/static/js/main.d81aa788.chunk.js"
+  },
+  {
+    "revision": "8c75bb50768a56e1ada0",
+    "url": "/monsters-roledex/static/js/runtime-main.4b740a68.js"
+  }
+]);
